135247ce1b09b89cbafa48000094b4f5864afe1c
leehohyung <hoppang250@gmail.com>
2025-01-05 23:15:04 +0900

    making clock system

---
+++ js/quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+    {
+        quote: "유명한 사람이 말한 명언01",
+        author: "유명한 사람01"
+    },
+    {
+        quote: "유명한 사람이 말한 명언02",
+        author: "유명한 사람02"
+    },
+    {
+        quote: "유명한 사람이 말한 명언03",
+        author: "유명한 사람03"
+    },
+    {
+        quote: "유명한 사람이 말한 명언04",
+        author: "유명한 사람04"
+    },
+    {
+        quote: "유명한 사람이 말한 명언05",
+        author: "유명한 사람05"
+    },
+    {
+        quote: "유명한 사람이 말한 명언06",
+        author: "유명한 사람06"
+    },
+    {
+        quote: "유명한 사람이 말한 명언07",
+        author: "유명한 사람07"
+    },
+    {
+        quote: "유명한 사람이 말한 명언08",
+        author: "유명한 사람08"
+    },
+    {
+        quote: "유명한 사람이 말한 명언09",
+        author: "유명한 사람09"
+    },
+    {
+        quote: "유명한 사람이 말한 명언10",
+        author: "유명한 사람10"
+    }
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[7]);
