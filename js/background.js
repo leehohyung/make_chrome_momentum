34f94766d8645f8a42bd63caf4dd81e0289acac6
leehohyung <hoppang250@gmail.com>
2025-01-08 23:39:25 +0900

    background wok is done!

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["bg_img_01.jpg", "bg_img_02.jpg", "bg_img_03.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+document.body.appendChild(bgImage);
+
+console.log(bgImage);
